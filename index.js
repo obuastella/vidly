607899a3f518d9b2a370b9454311f80e8a21f795
Obua Stella <stellaobua123@gmail.com>
2024-10-25 10:17:47 +0100

    GET all genres

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+// create the genres
+const genres = [
+  {
+    id: 1,
+    name: "Mystery",
+  },
+  {
+    id: 2,
+    name: "Comedy",
+  },
+  {
+    id: 3,
+    name: "Fiction",
+  },
+];
+
 // create the port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
@@ -10,4 +26,9 @@ app.listen(port, () => console.log(`Listening on Port ${port}...`));
 // return something on home page
 app.get("/", (req, res) => {
   res.status(200).send("Hello, world!");
+});
+
+// return all genres
+app.get("/genres", (req, res) => {
+  res.status(200).send(genres);
 });

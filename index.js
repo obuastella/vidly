3ecce7f8bdd5a4d8daa53d73d466ddf28c6ca022
Obua Stella <stellaobua123@gmail.com>
2024-10-25 13:07:59 +0100

    post request

--- index.js
+++ index.js
@@ -40,3 +40,23 @@ app.get("/genres/:id", (req, res) => {
     return res.status(404).send("This genre with the given id was not found");
   res.send(genre);
 });
+
+// post genre
+app.post("/genres", (req, res) => {
+  const { error } = validateGenre(req.body.name);
+  if (error) return res.status(404).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const result = schema.validate({ name: genre });
+  return result;
+}
